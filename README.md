1d8d62489b79cc52020bfe64c7d4550f897aa781
endre90 <endre90@gmail.com>
2022-02-14 00:54:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,84 @@
+## A ROS2 driver for the Robotiq 2f gripper
+Work in progress...
+
+Uses modbus protocol with RTU frame format for serial interfacing
+
+## Tested with:
+- Model: AGC-GRP-085-2016
+- OS: Linux
+- ROS version: Galactic (r2r v.0.6.2.)
+- Serial standard: RS-485
+
+## Preliminaries
+- sudo usermod -a -G dialout $USER
+- sudo reboot
+- Get the msg package from: todo...
+
+## Low level input mapping
+
+Byte 0: Gripper Status (gOBJ, gSTA, gGTO, gACT)\
+Byte 1: Reserved\
+Byte 2: Fault Status (kFLT, gFLT)\
+Byte 3: Position Request Echo (gPR)\
+Byte 4: Position (gPO)\
+Byte 5: Current (gCU)
+
+### Byte 0: Gripper Status
+- 0: gACT - Activation status, echo of the rACT bit (activation bit).
+    - 0x0 - Gripper reset.
+    - 0x1 - Gripper activation.
+- 1: Reserved
+- 2: Reserved
+- 3: gGTO - Action status, echo of the rGTO bit (go to bit).
+    - 0x0 - Stopped (or performing activation / automatic release).
+    - 0x1 - Go to Position Request.
+- 4, 5: gSTA - Gripper status, returns the current status & motion of the Gripper fingers.
+    - 0x00 - Gripper is in reset ( or automatic release ) state. See Fault Status if Gripper is activated.
+    - 0x01 - Activation in progress.
+    - 0x02 - Not used.
+    - 0x03 - Activation is completed.
+- 6, 7: gOBJ - Object detection status, is a built-in feature that provides information on possible object pick-up. Ignore if gGTO == 0.
+    - 0x00 - Fingers are in motion towards requested position. No object detected.
+    - 0x01 - Fingers have stopped due to a contact while opening before requested position. Object detected opening.
+    - 0x02 - Fingers have stopped due to a contact while closing before requested position. Object detected closing.
+    - 0x03 - Fingers are at requested position. No object detected or object has been loss / dropped.
+
+### Byte 1: Reserved
+
+### Byte 2: Fault Status
+- 0-3: gFLT: Fault status returns general error messages that are useful for troubleshooting. Fault LED (red) is present on the Gripper chassis, LED can be blue, red or both and be solid or blinking.
+    - 0x00 - No fault (LED is blue)
+    - Priority faults (LED is blue)
+        - 0x05 - Action delayed, activation (reactivation) must be completed prior to perfmoring the action.
+        - 0x07 - The activation bit must be set prior to action.
+    - Minor faults (LED continuous red)
+        - 0x08 - Maximum operating temperature exceeded, wait for cool-down.
+        - 0x09 - No communication during at least 1 second.
+    - Major faults (LED blinking red/blue) - Reset is required (rising edge on activation bit rACT needed).
+        - 0x0A - Under minimum operating voltage.
+        - 0x0B - Automatic release in progress.
+        - 0x0C - Internal fault; contact support@robotiq.com.
+        - 0x0D - Activation fault, verify that no interference or other error occurred.
+        - 0x0E - Overcurrent triggered.
+        - 0x0F - Automatic release completed.
+- 3-7: kFLT - See your optional Controller Manual (input registers & status).
+
+### Byte 3: Position request echo
+- 0-7: gPR - Echo of the requested position for the Gripper, value between 0x00 and 0xFF
+    - 0x00 - Full opening.
+    - 0xFF - Full closing.
+
+### Byte 4: Position
+- 0-7: gPO: Actual position of the Gripper obtained via the encoders, value between 0x00 and 0xFF.
+    - 0x00 - Fully opened.
+    - 0xFF - Fully closed.
+
+### Byte 5: Current
+- 0-7: gCU: The current is read instantaneously from the motor drive, value between 0x00 and 0xFF, approximate current equivalent is 10 * value read in mA.
+
+## High level input mapping
+todo
+## Low level output mapping
+todo
+## High level output mapping
+todo
---
+++ robotiq_2f_driver/src/main.rs
@@ -0,0 +1,237 @@
+use std::cmp::*;
+use std::{thread, time};
+use tokio_modbus::client::Context;
+use tokio_modbus::prelude::*;
+use tokio_serial::SerialStream;
+
+#[derive(Debug, Default)]
+struct MeasuredState {
+    pub valid: bool, // some unwrap failed, disregard state
+    pub g_act: u8,   // activation status
+    pub g_gto: u8,   // action status
+    pub g_sta: u8,   // gripper status
+    pub g_obj: u8,   // object detection status
+    pub g_flt: u8,   // fault status
+    pub g_pr: u8,    // position request echo
+    pub g_po: u8,    // position
+    pub g_cu: u8,    // current
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+struct CommandState {
+    pub r_act: u8, // action request
+    pub r_gto: u8, // go to request
+    pub r_atr: u8, // automatic release routine
+    pub r_ard: u8, // automatic release direction
+    pub r_pr: u8,  // position request
+    pub r_sp: u8,  // speed
+    pub r_fr: u8,  // force
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let ctx = r2r::Context::create()?;
+    let mut node = r2r::Node::create(ctx, "robotiq_2f_driver", "")?;
+
+    let handle = std::thread::spawn(move || loop {
+        node.spin_once(std::time::Duration::from_millis(100));
+    });
+
+    let tty_path = "/dev/ttyUSB0";
+
+    match modbus_client(tty_path).await {
+        Some(mut client) => {
+            tokio::task::spawn(async move {
+                initialize_on_startup(&mut client).await;
+                loop {
+                    println!("{:?}", read_state(&mut client).await);
+                    thread::sleep(time::Duration::from_millis(10));
+                    // write_state(&mut client, active_state).await;
+                }
+            });
+        }
+        None => {
+            r2r::log_error!(
+                "robotiq_2f_driver",
+                "Gripper couldn't connect to path: {}",
+                tty_path,
+            );
+        }
+    }
+
+    handle.join().unwrap();
+
+    Ok(())
+}
+
+async fn modbus_client(tty_path: &str) -> Option<tokio_modbus::client::Context> {
+    let tty_path = tty_path.to_string();
+    let builder = tokio_serial::new(tty_path, 115200);
+    let port = SerialStream::open(&builder);
+    match port {
+        Ok(stream) => match rtu::connect_slave(stream, Slave(0x0009)).await {
+            Ok(ctx) => Some(ctx),
+            Err(e) => {
+                r2r::log_error!(
+                    "robotiq_2f_driver",
+                    "RTU connecting to slave failed with {}.",
+                    e
+                );
+                None
+            }
+        },
+        Err(e) => {
+            r2r::log_error!(
+                "robotiq_2f_driver",
+                "Serial port builder failed with {}.",
+                e
+            );
+            None
+        }
+    }
+}
+
+async fn read_state(ctx: &mut Context) -> MeasuredState {
+    let mut state = vec![];
+    match ctx.read_holding_registers(0x07D0, 3).await {
+        Ok(response) => {
+            response.iter().for_each(|x| {
+                state.push((x & 0xFF00).rotate_right(8));
+                state.push(x & 0x00FF)
+            });
+            true
+        }
+        Err(e) => {
+            r2r::log_warn!(
+                "robotiq_2f_driver",
+                "Reading the holding registers failed with {}.",
+                e
+            );
+            false
+        }
+    };
+
+    MeasuredState {
+        valid: true,
+        g_act: (state[0] as u8 >> 0) & 0x01,
+        g_gto: (state[0] as u8 >> 3) & 0x01,
+        g_sta: (state[0] as u8 >> 4) & 0x03,
+        g_obj: (state[0] as u8 >> 6) & 0x03,
+        g_flt: state[2] as u8,
+        g_pr: state[3] as u8,
+        g_po: state[4] as u8,
+        g_cu: state[5] as u8,
+    }
+}
+
+async fn normalize_command_state(command: CommandState) -> CommandState {
+    CommandState {
+        r_act: min(max(command.r_act, 0), 1),
+        r_gto: min(max(command.r_gto, 0), 1),
+        r_atr: min(max(command.r_atr, 0), 1),
+        r_ard: min(max(command.r_ard, 0), 1),
+        r_pr: min(max(command.r_pr, 0), 255),
+        r_sp: min(max(command.r_sp, 0), 255),
+        r_fr: min(max(command.r_fr, 0), 255),
+    }
+}
+
+async fn write_state(ctx: &mut Context, command: CommandState) -> () {
+    match build_command_state(command).await {
+        Some(state) => match ctx.write_multiple_registers(0x03E8, &state).await {
+            Ok(()) => (),
+            Err(e) => {
+                r2r::log_warn!(
+                    "robotiq_2f_driver",
+                    "Writing multiple registers failed with {}.",
+                    e
+                );
+                ()
+            }
+        },
+        None => {
+            r2r::log_warn!("robotiq_2f_driver", "Building command state failed.");
+            ()
+        }
+    }
+}
+
+async fn build_command_state(command: CommandState) -> Option<[u16; 3]> {
+    let norm = normalize_command_state(command).await;
+    let bytes_msg = match norm.r_act.checked_add(norm.r_gto << 3) {
+        Some(pre_x) => match pre_x.checked_add(norm.r_atr << 4) {
+            Some(x) => Some([x, 0, 0, norm.r_pr, norm.r_sp, norm.r_fr]),
+            None => {
+                r2r::log_warn!(
+                    "robotiq_2f_driver",
+                    "Checked integer addition failed, overflow occurred."
+                );
+                None
+            }
+        },
+        None => {
+            r2r::log_warn!(
+                "robotiq_2f_driver",
+                "Checked integer addition failed, overflow occurred."
+            );
+            None
+        }
+    };
+
+    match bytes_msg {
+        Some([a, b, c, d, e, f]) => match (a as u16).rotate_right(8).checked_add(b as u16) {
+            Some(reg_1) => match (c as u16).rotate_right(8).checked_add(d as u16) {
+                Some(reg_2) => match (e as u16).rotate_right(8).checked_add(f as u16) {
+                    Some(reg_3) => Some([reg_1, reg_2, reg_3]),
+                    None => {
+                        r2r::log_warn!(
+                            "robotiq_2f_driver",
+                            "Checked integer addition failed, overflow occurred."
+                        );
+                        None
+                    }
+                },
+                None => {
+                    r2r::log_warn!(
+                        "robotiq_2f_driver",
+                        "Checked integer addition failed, overflow occurred."
+                    );
+                    None
+                }
+            },
+            None => {
+                r2r::log_warn!(
+                    "robotiq_2f_driver",
+                    "Checked integer addition failed, overflow occurred."
+                );
+                None
+            }
+        },
+        None => {
+            r2r::log_warn!(
+                "robotiq_2f_driver",
+                "Checked integer addition failed, overflow occurred."
+            );
+            None
+        }
+    }
+}
+
+async fn initialize_on_startup(ctx: &mut Context) -> () {
+    let reset_state = CommandState {
+        r_act: 0,
+        ..Default::default()
+    };
+
+    write_state(ctx, reset_state).await;
+
+    let active_state = CommandState {
+        r_act: 1,
+        r_gto: 1,
+        r_sp: 255,
+        r_fr: 150,
+        ..Default::default()
+    };
+
+    write_state(ctx, active_state).await;
+}
